4280794414b396f61e2ae6a35b75ab34e8c62458
unknown <lakshmanpalitha@gmail.com>
2015-07-04 08:40:13 +0530

    index template added with include

---
+++ template/includes/head.php
@@ -0,0 +1,20 @@
+<title>
+    Kupon - Deals and Coupons bootstrap template
+  </title>
+  <meta name="generator" content="#" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/themify-icons.css" rel="stylesheet">
+  <link href="css/font-awesome.css" rel="stylesheet">
+  <link href="owl.carousel/assets/owl.carousel.css" rel="stylesheet">
+  <link href="css/animate.min.css" rel="stylesheet">
+  <link href="css/animsition.css" rel="stylesheet">
+  <link href="css/plugins.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <!--[if lt IE 9]>
+<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+  <link rel="shortcut icon" href="#">
+  <link rel="apple-touch-icon" href="#">
+  <link rel="apple-touch-icon" sizes="72x72" href="#">
+  <link rel="apple-touch-icon" sizes="114x114" href="#">
