56ddbbcd28afe33a40b0ed7ca0ef560345c6d798
lakshmanpalitha <lakshmanpalitha@gmail.com>
2015-07-19 03:42:43 +0530

    profile page

---
+++ template/login.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+  
+<!-- Mirrored from pamukovic.com/demo/kupon/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 23 Jun 2015 05:34:34 GMT -->
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <meta charset="utf-8">
+  <?php include "includes/head.php"; ?>
+  </head>
+  
+  <body>
+    <div class="site-wrapper animsition" data-animsition-in="fade-in" data-animsition-out="fade-out">
+      <header>
+        <?php include "includes/topbar.php"; ?>
+        <?php include "includes/nav.php"; ?>
+          
+          <!-- /#nav wrap -->
+      </header>
+      <?php include "includes/search.php"; ?>
+      <!-- /.search form -->
+        <section id="page" class="container mTop-30 mBtm-50">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="panel-body frameLR bg-white shadow space-sm">
+
+                        <div class="col-md-6">
+                            <h3 class="dark-grey">
+                                Login
+                            </h3>
+
+
+                            <div class="form-group">
+                                <label>
+                                    Email Address
+                                </label>
+                                <input type="" name="" class="form-control" id="" value="">
+                            </div>
+                            <div class="form-group">
+                                <label>
+                                    Password
+                                </label>
+                                <input type="password" name="" class="form-control" id="" value="">
+                            </div>
+                            <P><a href="" >Forgot my passowrd<a></a></P>
+                        </div>
+
+                        <div class="mBtm-20 visible-xs">
+                        </div>
+
+                        <div class="col-md-6">
+                            <h3 class="dark-grey">
+                                or Register
+                            </h3>
+
+
+                            <button type="submit" class="btn btn-primary btn-raised ripple-effect">
+                                Create Account
+                            </button>
+                        </div>
+
+
+                    </div>
+                    <!-- /inner wrap -->
+                </div>
+            </div>
+
+        </section>
+
+
+
+      <!-- /#page ends -->
+      <div class="cta-box bg-blue-1 clearfix">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-3 col-sm-3 col-xs-12 pull-right">
+              <a href="#" class="btn btn-raised btn-primary ripple-effect btn-lg" data-original-title="" title="">
+                <i class="ti-shopping-cart">
+                </i>
+                &nbsp; Find your bid Now
+              </a>
+            </div>
+            <div class="col-md-9 col-sm-9 col-xs-12">
+              <h3>
+                Welcome to Lansuwa..
+              </h3>
+              <p>
+                Carefully designed to bring you the best performance, usage and customization experience!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /.CTA -->
+<?php include "includes/footer.php"; ?>      
+  </div>
+  <!-- /animitsion -->
+  <!-- JS files -->
+  <?php include "includes/js.php"; ?>
+ 
+  
+  </body>
+
+</html>
---
+++ template/my-account.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+  
+<!-- Mirrored from pamukovic.com/demo/kupon/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 23 Jun 2015 05:34:34 GMT -->
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <meta charset="utf-8">
+  <?php include "includes/head.php"; ?>
+  </head>
+  
+  <body>
+    <div class="site-wrapper animsition" data-animsition-in="fade-in" data-animsition-out="fade-out">
+      <header>
+        <?php include "includes/topbar.php"; ?>
+        <?php include "includes/nav.php"; ?>
+          
+          <!-- /#nav wrap -->
+      </header>
+      <?php include "includes/search.php"; ?>
+      <!-- /.search form -->
+
+      <section id="page" class="container mBtm-50">
+      <div class="row">
+      <div class="col-sm-12 clearfix mTop-30">
+      <div class="row">
+      <div class="col-lg-12">
+      <div class="hpanel">
+
+      <div class="panel-body frameLR bg-white shadow">
+
+      <form name="simpleForm" novalidate="" id="simpleForm" action="#" method="post">
+      <div class="row">
+          <div class=" mTop-30 col-sm-9 col-sm-offset-3" id="wizardControl">
+              <div class="wizNav">
+
+                  <a class="btn btn-primary btn-raised ripple-effect" href="#step1" data-toggle="tab">
+                     <span class="ti-package"></span>&nbsp;&nbsp;My Bids
+                  </a>
+                  <a class="btn btn-default btn-raised ripple-effect" href="#step2" data-toggle="tab">
+                      <span class="ti-cup"></span>&nbsp;&nbsp;win product
+                  </a>
+                  <a class="btn btn-default btn-raised ripple-effect" href="#step3" data-toggle="tab">
+                      <span class="ti-user"></span>&nbsp;&nbsp;My Profile
+                  </a>
+              </div>
+          </div>
+      </div>
+
+      <div class="tab-content">
+      <div id="step1" class="tab-pane active">
+
+      <table class="cart-contents">
+      <thead>
+      <tr>
+          <th class="hidden-xs">
+              Image
+          </th>
+          <th>
+              Description
+          </th>
+          <th>
+              Bids
+          </th>
+
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+          <td class="image hidden-xs">
+              <img src="images/affiliate-13.jpg" alt="product">
+          </td>
+          <td class="details">
+              <div class="clearfix">
+                  <div class="pull-left">
+                      <a href="#" class="title">
+                          Sirenis Punta Cana Resort Casino
+                      </a>
+                  </div>
+              </div>
+          </td>
+          <td class="qty">
+              3
+          </td>
+      </tr>
+      <tr>
+          <td class="image hidden-xs">
+              <img src="images/affiliate-13.jpg" alt="product">
+          </td>
+          <td class="details">
+              <div class="clearfix">
+                  <div class="pull-left">
+                      <a href="#" class="title">
+                          Sirenis Punta Cana Resort Casino
+                      </a>
+                  </div>
+              </div>
+          </td>
+          <td class="qty">
+              3
+          </td>
+      </tr>
+      <tr>
+          <td class="image hidden-xs">
+              <img src="images/affiliate-13.jpg" alt="product">
+          </td>
+          <td class="details">
+              <div class="clearfix">
+                  <div class="pull-left">
+                      <a href="#" class="title">
+                          Sirenis Punta Cana Resort Casino
+                      </a>
+                  </div>
+              </div>
+          </td>
+          <td class="qty">
+              3
+          </td>
+      </tr>
+
+
+      </tbody>
+
+      </table>
+
+      </div>
+
+      <div id="step2" class="tab-pane">
+
+          <div class="row">
+              <div class="col-sm-8 col-sm-offset-2">
+
+                  <div class="alert alert-success alert-dismissible ripple-effect btn-raised" role="alert">
+                      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                                <span aria-hidden="true">
+                                      ×
+                                    </span>
+                      </button>
+                      <strong>
+                          Oops!
+                      </strong>
+                      Thank you ! You have not win product!
+                  </div>
+              </div>
+
+          </div>
+
+
+      </div>
+      <div id="step3" class="tab-pane profile-wrapper">
+          <div class="row text-center m-t-lg m-b-lg">
+              <div id="step1" class="tab-pane active">
+
+                  <div class="row wizForm">
+                      <div class="col-lg-3">
+
+                          <h5>
+                              Hallo Lakshman
+                          </h5>
+                          <div class="profile-image-wapper shadow"><img src="https://scontent-sin1-1.xx.fbcdn.net/hphotos-xft1/v/t1.0-9/11745506_1668969550005332_298184417205816095_n.png?oh=0825f169d338f53488128b8adb3610a4&oe=565A1542" alt="Profile"></div>
+                          <div class="change-image-pop">
+                              <a class="btn btn-danger btn-xs btn-raised ripple-effect " href="#">
+                                  Change Image
+                              </a>
+                          </div>
+                      </div>
+                      <div class="col-lg-9">
+                          <div class="row">
+                              <div class="form-group col-lg-12">
+                                  <label class="col-lg-2">
+                                      Name
+                                  </label>
+                                  <div class="col-lg-10">
+                                      <input type="" value="" id="" class="form-control" name="name" placeholder="Name">
+                                  </div>
+                              </div>
+
+                              <div class="form-group col-lg-12">
+                                  <label class="col-lg-2">
+                                      E-Mail
+                                  </label>
+                                  <div class="col-lg-10">
+                                   <input type="" value="" id="" class="form-control" name="email" placeholder="Email">
+                                   </div>
+                              </div>
+                              <div class="form-group col-lg-12">
+                                  <label class="col-lg-2">
+                                      Telphone
+                                  </label>
+                                  <div class="col-lg-10">
+                                  <input type="" value="" id="" class="form-control" name="Telphone" placeholder="Telphone">
+                                   </div>
+                              </div>
+                              <div class="form-group col-lg-12">
+                                  <label class="col-lg-2">
+                                      NIC number
+                                  </label>
+                                  <div class="col-lg-10">
+                                  <input type="" value="" id="" class="form-control" name="nic" placeholder="NIC">
+                                   </div>
+                              </div>
+                              <div class="form-group col-lg-12">
+                                  <label class="col-lg-2">
+                                      Address
+                                  </label>
+                                  <div class="col-lg-10">
+                                      <textarea id="contact_message" class="form-control" name="message"  rows="6">  </textarea>
+                                   </div>
+                              </div>
+                              <div class="form-group col-lg-12">
+                                  <label class="col-lg-2">
+                                      Password
+                                  </label>
+                                  <div class="col-lg-10">
+                                      <a class="btn btn-danger btn-raised ripple-effect prev" href="#">
+                                          Change password
+                                      </a>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+
+
+              </div>
+          </div>
+          <div class="text-right col-sm-12">
+
+              <button class="btn btn-primary btn-raised ripple-effect"> Save Profile </button>
+          </div>
+      </div>
+      </div>
+      <div class="mBtm-50 clearfix">
+      </div>
+      </form>
+
+
+
+      </div>
+      </div>
+      </div>
+
+      </div>
+      </div>
+      <!-- /inner wrap -->
+      </div>
+
+
+      </section>
+
+      <!-- /#page ends -->
+      <div class="cta-box bg-blue-1 clearfix">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-3 col-sm-3 col-xs-12 pull-right">
+              <a href="#" class="btn btn-raised btn-primary ripple-effect btn-lg" data-original-title="" title="">
+                <i class="ti-shopping-cart">
+                </i>
+                &nbsp; Find your bid Now
+              </a>
+            </div>
+            <div class="col-md-9 col-sm-9 col-xs-12">
+              <h3>
+                Welcome to Lansuwa..
+              </h3>
+              <p>
+                Carefully designed to bring you the best performance, usage and customization experience!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /.CTA -->
+<?php include "includes/footer.php"; ?>      
+  </div>
+  <!-- /animitsion -->
+  <!-- JS files -->
+  <?php include "includes/js.php"; ?>
+ 
+  
+  </body>
+
+</html>
