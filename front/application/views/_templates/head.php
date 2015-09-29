c54ec055d3defc9b3eca36f18173c68319acc4f2
lakshmanpalitha <lakshmanpalitha@gmail.com>
2015-09-29 23:23:14 +0530

    google analatycs code added

--- front/application/views/_templates/head.php
+++ front/application/views/_templates/head.php
@@ -14,6 +14,16 @@
         font-style: normal;
     }
 </style>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-68212937-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
 <link href="<?php echo URL . FRONTEND ?>public/css/bootstrap.min.css" rel="stylesheet">
 <link href="<?php echo URL . FRONTEND ?>public/css/themify-icons.css" rel="stylesheet">
 <link href="<?php echo URL . FRONTEND ?>public/css/font-awesome.css" rel="stylesheet">
